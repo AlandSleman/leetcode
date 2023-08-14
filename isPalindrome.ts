c08ea4bfa2277b6d125deb265c97cdae7d64a9bf
AlandSleman <alandsazkm@gmail.com>
2023-08-14 11:50:13 +0300

    palindrone

---
+++ isPalindrome.ts
@@ -0,0 +1,13 @@
+type S = "I" | "V" | "X" | "L" | "C" | "D" | "M";
+function romanToInt(s: S): number {
+  let result = 0;
+  if (s === "I") 
+}
+console.log(romanToInt("I"));
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
